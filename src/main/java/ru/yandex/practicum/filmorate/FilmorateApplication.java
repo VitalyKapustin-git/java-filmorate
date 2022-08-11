5f7db7c400c7778e33cf89f1480300f01bc4bc2f
vkapustin <vkapustindtln@yandex.ru>
2022-08-12 02:12:55 +0300

    feat: - removed notations

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,10 +2,8 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 @SpringBootApplication
-@AutoConfigureTestDatabase
 public class FilmorateApplication {
 
 	public static void main(String[] args) {
